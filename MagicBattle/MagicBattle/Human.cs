982d8f7f38d55ab7dfa40d4d115d18e17f97f4bf
Serhii Horodov <serhii.horodov@gmail.com>
2022-04-25 19:17:10 +0300

    add new propertyes in Human class

--- MagicBattle/MagicBattle/Human.cs
+++ MagicBattle/MagicBattle/Human.cs
@@ -1,20 +1,30 @@
 ﻿namespace MagicBattle
 {
-    public class Human
+  public class Human
+  {
+    public string Name { get; protected set; }
+
+    public int Age { get; protected set; }
+
+    public double HealthValue { get; set; }
+
+    public double ManaValue { get; set; }
+
+    public bool IsHasMana { get; set; }
+
+    // check if magician know or not the spell
+    public bool IsKnownSpell { get; set; } = false;
+
+
+    public Human(int age, string name)
     {
-        public string Name { get; protected set; }
-
-        public int Age { get; protected set; }
-
-        public Human(int age, string name)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public override string ToString()
-        {
-            return $"Name = {Name}, Age = {Age}";
-        }
+      Name = name;
+      Age = age;
     }
+
+    public override string ToString()
+    {
+      return $"Name = {Name}, Age = {Age}";
+    }
+  }
 }
