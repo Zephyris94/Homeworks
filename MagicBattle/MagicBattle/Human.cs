adf4180e2fd9a47cf8bbaf0662accec15d55d848
Serhii Horodov <serhii.horodov@nure.ua>
2022-05-03 15:15:35 +0300

    merge test

--- MagicBattle/MagicBattle/Human.cs
+++ MagicBattle/MagicBattle/Human.cs
@@ -18,33 +18,15 @@
 
     public Human(int age, string name)
     {
-<<<<<<< HEAD
       Name = name;
       Age = age;
-=======
-        public string Name { get; protected set; }
-
-        public int Age { get; protected set; }
+    }
 
         public Human()
         {
-            
-        }
-
-        public Human(int age, string name)
-        {
-            Name = name;
-            Age = age;
         }
 
         public override string ToString()
-        {
-            return $"Name = {Name}, Age = {Age}";
-        }
->>>>>>> Test
-    }
-
-    public override string ToString()
     {
       return $"Name = {Name}, Age = {Age}";
     }
--- MagicBattle/MagicBattle/Spells/Spell.cs
+++ MagicBattle/MagicBattle/Spells/Spell.cs
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MagicBattle.Spells
-{
-  public class Spell
-  {
-    public double MinPower { get; set; } = 100;
-
-    public double MaxPower { get; set; } = 1000; 
-
-    public string Effect { get; set; }
-
-    public double Damage { get; set; } = 10;
-
-
-    public int Duration { get; set; } = 10;
-
-    // min level for magician who can use this spell    
-    public int MinMagicianLevel { get; private set; } = 1;
-
-    // min mana level that need magician for use spell 
-    public int MinManaValue { get; private set; } = 5;
-    
-
-
-    public enum Type
-    {      
-      // regenerate health or mana units
-      Regenerative = 1,
-
-      // can protect from damage (shield or something else)     
-      Protective = 2,
-
-      // can damage enemy
-      Destructive = 3,
-      
-      // other
-      Versative = 4
-    }
-
-    public enum Complexity
-    {     
-      Light = 1, 
-
-      Middle = 2,
-
-      Hard = 3,
-
-      Fatal = 4    
-      
-    }
-  }
-=======
 ﻿using MagicBattle.Spells.Metadata;
 
 namespace MagicBattle.Spells
@@ -76,5 +20,4 @@ namespace MagicBattle.Spells
 
         public abstract string GetDescription();
     }
->>>>>>> Test
 }
